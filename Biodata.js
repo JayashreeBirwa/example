848fb925be8ebf1dc17e2e1d791c2ab23a3b0a81
JayashreeBirwa <123164836+JayashreeBirwa@users.noreply.github.com>
2023-01-22 21:31:15 +0530

    3

--- Biodata.js
+++ Biodata.js
@@ -55,3 +55,31 @@ function App() {
 export default App;
 
 
+Fetch
+
+import React, { useState } from "react";
+//import Axios from 'axios';
+import './App.css';
+
+function App() {
+  const[joke, setJoke]=useState("");
+
+  const getJoke=()=> {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+     .then((response)=> response.json())
+     .then((data)=> {
+      setJoke(data.setup+"..."+data.punchline);
+     });
+  };
+
+  return (
+    <div>
+      Hello YouTube<button onClick={getJoke}>GetJoke Right Now</button>
+      {joke}
+    </div>
+  );
+}
+
+export default App;
+
+
